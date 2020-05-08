3e3ca1b788589c55c5c0f32a1a07dbbdfbde5e6b
guseyn <guseynism@gmail.com>
2020-05-08 10:53:45 +0300

    add tests on xml head

---
+++ src/main/java/com/guseyn/broken_xml/UnicodeOfChar.java
@@ -0,0 +1,12 @@
+package com.guseyn.broken_xml;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UnicodeOfChar {
+
+    private Map<Character, Integer> codes = new HashMap<Character, Integer>(){{
+       // put()
+    }};
+
+}
--- src/test/java/ParsedXmlTest.java
+++ src/test/java/ParsedXmlTest.java
@@ -1,13 +1,30 @@
+import com.guseyn.broken_xml.Attribute;
 import com.guseyn.broken_xml.ParsedXML;
+import com.guseyn.broken_xml.XmlDocument;
+import com.guseyn.broken_xml.XmlHeadElement;
 import java.io.IOException;
+import java.util.List;
 import org.apache.commons.io.IOUtils;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ParsedXmlTest {
     @Test
-    public void test() throws IOException, NoSuchFieldException, IllegalAccessException {
-        final ParsedXML xml = new ParsedXML(dataByPath("valid-xml.xml"));
-        xml.value();
+    public void justXmlHead() throws IOException, NoSuchFieldException, IllegalAccessException {
+        final ParsedXML xml = new ParsedXML(dataByPath("just-xml-head.xml"));
+        XmlDocument doc = xml.value();
+        assertEquals(doc.heads().size(), 1);
+        assertXmlHead(doc.heads().get(0), "1.0");
+    }
+
+    @Test
+    public void multipleXmlHeads() throws IOException, NoSuchFieldException, IllegalAccessException {
+        final ParsedXML xml = new ParsedXML(dataByPath("multiple-xml-heads.xml"));
+        XmlDocument doc = xml.value();
+        assertEquals(doc.heads().size(), 3);
+        assertXmlHead(doc.heads().get(0), "1.0");
+        assertXmlHead(doc.heads().get(1), "2.0");
+        assertXmlHead(doc.heads().get(2), "3.0");
     }
 
     private String dataByPath(final String path) throws IOException {
@@ -15,5 +32,19 @@ public class ParsedXmlTest {
             this.getClass().getResourceAsStream(path),
             "UTF-8"
         ).concat("\n");
+    }
+
+    private void assertXmlHead(XmlHeadElement head, String version) {
+        List<Attribute> attributes = head.attributes();
+        assertEquals(attributes.size(), 3);
+        Attribute firstAttribute = attributes.get(0);
+        Attribute secondAttribute = attributes.get(1);
+        Attribute thirdAttribute = attributes.get(2);
+        assertEquals(firstAttribute.name(), "version");
+        assertEquals(firstAttribute.value(), version);
+        assertEquals(secondAttribute.name(), "encoding");
+        assertEquals(secondAttribute.value(), "UTF-8");
+        assertEquals(thirdAttribute.name(), "attribute");
+        assertEquals(thirdAttribute.value(), "value");
     }
 }
