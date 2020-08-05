f96a9d7b51a8d87a4877ef3b6a27452fdcdfa317
guseyn <guseynism@gmail.com>
2020-08-05 10:41:07 +0300

    case on null pointer

---
+++ src/test/java/RealTestForNullPointer.java
@@ -0,0 +1,20 @@
+import com.guseyn.broken_xml.ParsedXML;
+import com.guseyn.broken_xml.XmlDocument;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
+
+public class RealTestForNullPointer extends XmlSource {
+    @Test
+    @Override
+    void test() throws IOException {
+        final ParsedXML xml = new ParsedXML(
+            dataByPath("real-test-for-null-pointer.xml")
+        );
+        XmlDocument document = xml.document();
+
+        assertEquals(document.roots().size(), 4);
+        assertNull(document.roots().get(3));
+    }
+}
